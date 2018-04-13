73402a08516440ad40ad15310b380aba5465e4bf
obscure12 <longwangc12@gmail.com>
2018-04-13 21:52:56 +0900

    プロジェクト雛型作成

---
+++ ConsoleRpg/src/main/java/com/consolerpg/Main.java
@@ -0,0 +1,17 @@
+package com.consolerpg;
+
+/**
+ * コンソールRPGのメインクラス
+ * @author obscure12
+ *
+ */
+public class Main {
+
+	/**
+	 * mainメソッド
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World From Console RPG");
+	}
+}
